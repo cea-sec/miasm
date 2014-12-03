2ce28c9d3ac0b20b1b6ceb19ddccdac3f187b679
Camille Mougey <camille.mougey@cea.fr>
2014-12-03 21:48:04 +0100

    UnpackUPX: remove unused variable

--- example/unpack_upx.py
+++ example/unpack_upx.py
@@ -88,12 +88,11 @@ def update_binary(jitter):
 # Set callbacks
 sb.jitter.add_breakpoint(end_label, update_binary)
 
-
+# Run
 sb.run()
 
-regs = sb.jitter.cpu.get_gpreg()
+# Rebuild PE
 new_dll = []
-# XXXXX
 
 sb.pe.SHList.align_sections(0x1000, 0x1000)
 logging.info(repr(sb.pe.SHList))
