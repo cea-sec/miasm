aa6165b051e9bd6363b759a04babfc558099fed8
Camille Mougey <camille.mougey@cea.fr>
2014-12-24 19:35:27 +0100

    Example: AsmBox: fix imports and rename e -> pe

--- example/asm_box_x86_32.py
+++ example/asm_box_x86_32.py
@@ -1,23 +1,22 @@
 #! /usr/bin/env python
+from pdb import pm
+
+from elfesteem import pe_init
 
 from miasm2.core.cpu import parse_ast
-from miasm2.arch.x86.arch import mn_x86, base_expr, variable
-from miasm2.core.bin_stream import bin_stream
+from miasm2.arch.x86.arch import mn_x86, base_expr
 from miasm2.core import parse_asm
 from miasm2.expression.expression import *
-from elfesteem import *
-from pdb import pm
 from miasm2.core import asmbloc
-import struct
 
-e = pe_init.PE()
-s_text = e.SHList.add_section(name="text", addr=0x1000, rawsize=0x1000)
-s_iat = e.SHList.add_section(name="iat", rawsize=0x100)
+pe = pe_init.PE()
+s_text = pe.SHList.add_section(name="text", addr=0x1000, rawsize=0x1000)
+s_iat = pe.SHList.add_section(name="iat", rawsize=0x100)
 new_dll = [({"name": "USER32.dll",
              "firstthunk": s_iat.addr}, ["MessageBoxA"])]
-e.DirImport.add_dlldesc(new_dll)
-s_myimp = e.SHList.add_section(name="myimp", rawsize=len(e.DirImport))
-e.DirImport.set_rva(s_myimp.addr)
+pe.DirImport.add_dlldesc(new_dll)
+s_myimp = pe.SHList.add_section(name="myimp", rawsize=len(pe.DirImport))
+pe.DirImport.set_rva(s_myimp.addr)
 
 reg_and_id = dict(mn_x86.regs.all_regs_ids_byname)
 
@@ -48,19 +47,19 @@ msg:
 ''')
 
 # fix shellcode addr
-symbol_pool.set_offset(symbol_pool.getby_name("main"), e.rva2virt(s_text.addr))
+symbol_pool.set_offset(symbol_pool.getby_name("main"), pe.rva2virt(s_text.addr))
 symbol_pool.set_offset(symbol_pool.getby_name_create("MessageBoxA"),
-                       e.DirImport.get_funcvirt('MessageBoxA'))
-e.Opthdr.AddressOfEntryPoint = s_text.addr
+                       pe.DirImport.get_funcvirt('MessageBoxA'))
+pe.Opthdr.AddressOfEntryPoint = s_text.addr
 
-for b in blocs[0]:
-    print b
+for bloc in blocs[0]:
+    print bloc
 
 resolved_b, patches = asmbloc.asm_resolve_final(
     mn_x86, blocs[0], symbol_pool)
 print patches
 
 for offset, raw in patches.items():
-    e.virt[offset] = raw
+    pe.virt[offset] = raw
 
-open('box_x86_32.bin', 'wb').write(str(e))
+open('box_x86_32.bin', 'wb').write(str(pe))
