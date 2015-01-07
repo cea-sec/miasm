bc8e9908fff38cdca1ab8d92679a85941d35e101
Camille Mougey <camille.mougey@cea.fr>
2015-01-07 17:47:46 +0100

    Example/Asm_x86_32_enc: Reorganize and remove unused imports

--- example/asm_box_x86_32_enc.py
+++ example/asm_box_x86_32_enc.py
@@ -1,14 +1,13 @@
 #! /usr/bin/env python
-
-from miasm2.core.cpu import parse_ast
-from miasm2.arch.x86.arch import mn_x86, base_expr, variable
-from miasm2.core.bin_stream import bin_stream
-from miasm2.core import parse_asm
-from miasm2.expression.expression import *
-from elfesteem import *
 from pdb import pm
+
+from elfesteem import pe_init
+
 from miasm2.core import asmbloc
-import struct
+from miasm2.core.cpu import parse_ast
+from miasm2.arch.x86.arch import mn_x86, base_expr
+from miasm2.core import parse_asm
+import miasm2.expression.expression as m2_expr
 
 e = pe_init.PE()
 s_text = e.SHList.add_section(name="text", addr=0x1000, rawsize=0x1000)
@@ -23,11 +22,11 @@ reg_and_id = dict(mn_x86.regs.all_regs_ids_byname)
 
 
 def my_ast_int2expr(a):
-    return ExprInt32(a)
+    return m2_expr.ExprInt32(a)
 
 
 def my_ast_id2expr(t):
-    return reg_and_id.get(t, ExprId(t, size=32))
+    return reg_and_id.get(t, m2_expr.ExprId(t, size=32))
 
 my_var_parser = parse_ast(my_ast_id2expr, my_ast_int2expr)
 base_expr.setParseAction(my_var_parser)
