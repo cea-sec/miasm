d3a8f781deb22070147f59a2e3e2c86af6560723
Camille Mougey <camille.mougey@cea.fr>
2015-01-07 17:57:06 +0100

    Example/Asm_x86: Reorganize and remove unused imports

--- example/asm_x86.py
+++ example/asm_x86.py
@@ -1,9 +1,9 @@
 #! /usr/bin/env python
 
 from miasm2.core.cpu import parse_ast
-from miasm2.arch.x86.arch import mn_x86, base_expr, variable
+from miasm2.arch.x86.arch import mn_x86, base_expr
 from miasm2.core import parse_asm
-from miasm2.expression.expression import *
+import miasm2.expression.expression as m2_expr
 from miasm2.core import asmbloc
 from elfesteem.strpatchwork import StrPatchwork
 
@@ -11,11 +11,11 @@ reg_and_id = dict(mn_x86.regs.all_regs_ids_byname)
 
 
 def my_ast_int2expr(a):
-    return ExprInt32(a)
+    return m2_expr.ExprInt32(a)
 
 
 def my_ast_id2expr(t):
-    return reg_and_id.get(t, ExprId(t, size=32))
+    return reg_and_id.get(t, m2_expr.ExprId(t, size=32))
 
 my_var_parser = parse_ast(my_ast_id2expr, my_ast_int2expr)
 base_expr.setParseAction(my_var_parser)
