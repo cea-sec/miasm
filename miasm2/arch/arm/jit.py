1bcfa58c0e3e54cdf9daa92590b2fa3935f4b319
Fabrice Desclaux <fabrice.desclaux@cea.fr>
2015-01-29 08:57:40 +0100

    Arch/Jit: remove useless custom lib_handler

--- miasm2/arch/arm/jit.py
+++ miasm2/arch/arm/jit.py
@@ -58,33 +58,6 @@ class jitter_arml(jitter):
             arg = self.get_stack_arg(n-4)
         return arg
 
-    def add_lib_handler(self, libs, user_globals=None):
-        """Add a function to handle libs call with breakpoints
-        @libs: libimp instance
-        @user_globals: dictionnary for defined user function
-        """
-        if user_globals is None:
-            user_globals = {}
-
-        from miasm2.os_dep import linux_stdlib
-
-        def handle_lib(jitter):
-            fname = libs.fad2cname[jitter.pc]
-            if fname in user_globals:
-                f = user_globals[fname]
-            elif fname in linux_stdlib.__dict__:
-                f = linux_stdlib.__dict__[fname]
-            else:
-                log.debug('%s' % repr(fname))
-                raise ValueError('unknown api', hex(jitter.pop_uint32_t()), repr(fname))
-            f(jitter)
-            jitter.pc = getattr(jitter.cpu, jitter.ir_arch.pc.name)
-            return True
-
-        for f_addr in libs.fad2cname:
-            self.add_breakpoint(f_addr, handle_lib)
-
-
     def init_run(self, *args, **kwargs):
         jitter.init_run(self, *args, **kwargs)
         self.cpu.PC = self.pc
