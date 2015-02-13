c6e218705278303ec06d418ed9e0548077c4517d
Fabrice Desclaux <fabrice.desclaux@cea.fr>
2015-02-13 11:18:23 +0100

    Sandbox: add x86_64 bit example

---
+++ example/jitter/sandbox_pe_x86_64.py
@@ -0,0 +1,23 @@
+import os
+from pdb import pm
+from miasm2.analysis.sandbox import Sandbox_Win_x86_64
+
+# Python auto completion
+filename = os.environ.get('PYTHONSTARTUP')
+if filename and os.path.isfile(filename):
+    execfile(filename)
+
+# Insert here user defined methods
+
+# Parse arguments
+parser = Sandbox_Win_x86_64.parser(description="PE sandboxer")
+parser.add_argument("filename", help="PE Filename")
+options = parser.parse_args()
+
+# Create sandbox
+sb = Sandbox_Win_x86_64(options.filename, options, globals())
+
+# Run
+sb.run()
+
+assert(sb.jitter.run is False)
