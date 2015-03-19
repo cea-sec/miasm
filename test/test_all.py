595fda5b35238a9e468af5d3d674129d62e369dc
Ajax <commial@gmail.com>
2015-03-19 10:42:03 +0100

    TestAll: Add a test for disasm/full without arch and addr to reach previously untested code

--- test/test_all.py
+++ test/test_all.py
@@ -236,6 +236,8 @@ for script, prods in [(["single_instr.py"], []),
                       (["function.py"], ["graph.txt"]),
                       (["file.py", Example.get_sample("box_upx.exe"),
                         "0x410f90"], ["graph.txt"]),
+                      (["full.py", Example.get_sample("box_upx.exe")],
+                       ["graph_execflow.txt", "lines.txt"]),
                       ]:
     testset += ExampleDisassembler(script, products=prods)
 
