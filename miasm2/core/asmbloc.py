730b6b04b70ecbfefa43e9287749768895b809c3
Tim Blazytko <tim.blazytko@rub.de>
2015-04-14 01:03:59 +0200

    make asm_label comparable

--- miasm2/core/asmbloc.py
+++ miasm2/core/asmbloc.py
@@ -59,6 +59,15 @@ class asm_label:
         rep += '>'
         return rep
 
+    def __eq__(self, other):
+        return self.__dict__ == other.__dict__
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
+
+    def __hash__(self):
+        return hash((self.fixedblocs, self.name, self.attrib, self.offset))
+
 class asm_raw:
     def __init__(self, raw=""):
         self.raw = raw
