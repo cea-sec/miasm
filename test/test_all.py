8ed048f3bd64e51b4a508d90255e4fe4fe530b87
Ajax <commial@gmail.com>
2015-04-21 17:25:58 +0200

    Depgraph: Add a new example `depgraph` calling DepGraph with options

--- test/test_all.py
+++ test/test_all.py
@@ -321,6 +321,15 @@ class ExampleSymbolExec(Example):
 
 
 testset += ExampleSymbolExec(["single_instr.py"])
+for options, nb_sol in [([], 8),
+                        (["-i"], 12)]:
+    testset += ExampleSymbolExec(["depgraph.py",
+                                  Example.get_sample("simple_test.bin"),
+                                  "-m", "x86_32", "0x0", "0x8b",
+                                  "eax"] + options,
+                                 products=["sol_%d.dot" % nb
+                                           for nb in xrange(nb_sol)])
+
 
 ## Jitter
 class ExampleJitter(Example):
