dc3d140b05047c310a2c94e3258bff6cc1ce81cb
Álvaro Felipe Melchor <alvaro.felipe91@gmail.com>
2016-10-05 16:54:27 +0200

    Fix build mac os x

--- miasm2/jitter/vm_mngr.h
+++ miasm2/jitter/vm_mngr.h
@@ -20,6 +20,8 @@
 
 #ifdef __APPLE__
 #define __BYTE_ORDER __BYTE_ORDER__
+#define __BIG_ENDIAN BIG_ENDIAN
+#define __LITTLE_ENDIAN LITTLE_ENDIAN
 #elif defined(__NetBSD__)
 #define __BYTE_ORDER _BYTE_ORDER
 #define __BIG_ENDIAN _BIG_ENDIAN
