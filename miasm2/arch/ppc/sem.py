def39b8ef0643c32b23cb41bb321ecc23435afd0
Fabrice Desclaux <fabrice.desclaux@cea.fr>
2018-07-18 18:45:11 +0200

    PPC: fix tipo

--- miasm2/arch/ppc/sem.py
+++ miasm2/arch/ppc/sem.py
@@ -98,7 +98,7 @@ def mn_do_and(ir, instr, ra, rs, arg2):
     return ret, []
 
 def mn_do_cntlzw(ir, instr, ra, rs):
-    ret = [ ExprAff(ra, ExprOp('cntleadzeros'), rs) ]
+    ret = [ ExprAff(ra, ExprOp('cntleadzeros', rs)) ]
 
     if instr.name[-1] == '.':
         ret += mn_compute_flags(rvalue)
