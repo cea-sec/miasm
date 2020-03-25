93c1e157c8459b0b8d47bbed41bc9fa66efdce89
William Bruneau <william.bruneau@epfedu.fr>
2020-03-25 13:16:09 +0100

    Fix Windows tests

--- miasm/jitter/interval_tree/rbtree.h
+++ miasm/jitter/interval_tree/rbtree.h
@@ -38,8 +38,7 @@ struct rb_node {
 	unsigned long  __rb_parent_color;
 	struct rb_node *rb_right;
 	struct rb_node *rb_left;
-} __attribute__((aligned(sizeof(long))));
-    /* The alignment might seem pointless, but allegedly CRIS needs it */
+};
 
 struct rb_root {
 	struct rb_node *rb_node;
