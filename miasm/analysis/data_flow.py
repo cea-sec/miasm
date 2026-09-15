15ed7ee41fbb46399723fca8463a86520deb7353
Johannes <johannes@bluebottle.gg>
2026-09-15 16:21:40 +0200

    Fix unsound DelDummyPhi for phis with overlapping equivalence classes

--- miasm/analysis/data_flow.py
+++ miasm/analysis/data_flow.py
@@ -1588,7 +1588,10 @@ class DelDummyPhi(object):
             if not irblock_has_phi(block):
                 continue
             assignblk = block[0]
+            handled_phis = set()
             for dst, phi_src in viewitems(assignblk):
+                if dst in handled_phis:
+                    continue
                 assert phi_src.is_op('Phi')
                 result = self.get_equivalence_class(dst, ids_to_src)
                 if result is None:
@@ -1602,6 +1605,8 @@ class DelDummyPhi(object):
                     continue
                 # We have an equivalence of nodes
                 to_del = set(defined)
+                assert len(to_del & handled_phis) == 0
+                handled_phis |= to_del
                 # Remove all implicated phis
                 for dst in to_del:
                     loc_key = def_to_loc[dst]
@@ -1615,6 +1620,7 @@ class DelDummyPhi(object):
                         fixed_phis[old_dst] = old_phi_src
 
                     assignblks = list(block)
+                    ids_to_src[dst] = true_value
                     assignblks[0] = AssignBlock(fixed_phis, assignblk.instr)
                     assignblks[1:1] = [AssignBlock({dst: true_value}, assignblk.instr)]
                     new_irblock = IRBlock(block.loc_db, block.loc_key, assignblks)
