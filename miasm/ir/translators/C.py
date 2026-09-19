46337e766b0055c437fa44b5fdd218bec8241532
kStor2poche <43518872+kStor2poche@users.noreply.github.com>
2026-09-19 10:19:39 +0200

    No need for the comment as it has actually been tested

--- miasm/ir/translators/C.py
+++ miasm/ir/translators/C.py
@@ -367,7 +367,6 @@ class TranslatorC(Translator):
                 return out
 
             elif expr.op == "segm":
-                # XXX: maybe verify but should allow things like gs:[-0x18] to work on 32bits
                 return f"(segm2addr(jitcpu, %s, %s) & {(1 << expr.size) - 1}ULL)" % (
                     self.from_expr(expr.args[0]),
                     self.from_expr(expr.args[1])
