bf65187b20556ef5c1448f9d24b77a37397ed2fa
Johannes <johannesmaier27@gmail.com>
2026-09-21 23:33:59 +0200

    Revert: Insert copies before IRDst

--- miasm/analysis/outofssa.py
+++ miasm/analysis/outofssa.py
@@ -5,7 +5,6 @@ from miasm.ir.ir import IRBlock, AssignBlock
 from miasm.analysis.ssa import get_phi_sources_parent_block, \
     irblock_has_phi
 
-import logging
 
 class Varinfo(object):
     """Store liveness information for a variable"""
@@ -89,13 +88,7 @@ class UnSSADiGraph(object):
             for parent, parallel_copies in viewitems(parent_to_parallel_copies):
                 parent = ircfg.blocks[parent]
                 assignblks = list(parent)
-                # insert before IRDst, TODO: might need to check for interference as mentioned in the paper
-                # sanity checks for now:
-                jmp_block = parent[-1]
-                for jmp_src in jmp_block.values():
-                    jmp_read_values = set(filter(lambda expr: expr.is_id(), jmp_src.get_r(mem_read=True)))
-                    assert len(jmp_read_values & set(parallel_copies.keys())) == 0, "Interference between jmp instruction read set and parallel copy write set"
-                assignblks.insert(-1, AssignBlock(parallel_copies, jmp_block.instr))
+                assignblks.append(AssignBlock(parallel_copies, parent[-1].instr))
                 new_irblock = IRBlock(parent.loc_db, parent.loc_key, assignblks)
                 ircfg.blocks[parent.loc_key] = new_irblock
 
